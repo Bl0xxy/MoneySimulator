710a7b366474aa02271f77a4e6e9157f679819aa
BloxxyVidsYT <99568449+BloxxyVidsYT@users.noreply.github.com>
2023-04-14 18:30:37 -0500

    Delete commands directory

--- commands/money.py
+++
@@ -1,6 +0,0 @@
-def main(stats):
-  stats['moneyamount'] += stats['multiplier']
-  return
-
-if __name__ == '__main__':
-    print("It doesn't work like that!  Try running main.py to play Money Simulator!")
